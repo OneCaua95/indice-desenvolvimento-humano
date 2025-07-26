0e7d4bba40ae5d29af549db3d869557a26195acc
Cauã Wendel <152828776+OneCaua95@users.noreply.github.com>
2025-07-26 19:55:47 -0300

    Update extract.py

--- extract.py
+++ extract.py
@@ -6,16 +6,6 @@ class Extract:
         self.data_cache = data_cache
         self.data = None
         
-    """
-    def extract_data(self):
-        file = 'https://www.undp.org/sites/g/files/zskgke326/files/2023-07/base_de_dados.xlsx'
-
-        df = read_excel(file)
-
-        return df
-
-    """ 
-
     def extract_data(self):
         file = 'data/base_de_dados.xlsx'
 
